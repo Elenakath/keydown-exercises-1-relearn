44a516272bacc278867a2064040080f31c714362
elena_e <elenaka123@gmail.com>
2024-04-03 12:24:30 -0500

    Inital commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var clear = document.querySelector("button");
+
+
+document.addEventListener("keydown", function (e) {
+    var key = e.key;
+    if (key === "1") {
+        box1.innerText = "🫶";
+    } else if (key === "2") {
+        box2.innerText = "⭐";
+    } else if (key === "3") {
+        box3.innerText = "😊";
+    }
+});
+
+clear.addEventListener("click", function (){
+    box1.innerText = "1";
+    box2.innerText = "2";
+    box3.innerText = "3";
+});
